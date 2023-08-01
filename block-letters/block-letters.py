8a6b0e6d4b128abaaa280ac448ed57dd36185428
Joao Astolfi <jpastolfi@gmail.com>
2023-08-01 15:58:57 +0100

    block-letters exercise

---
+++ block-letters/block-letters.py
@@ -0,0 +1,12 @@
+# João Pedro
+# Studying Python to pass the BPI evaluation
+
+print("""
+JJJJJ   PPPP
+  J     P   P
+  J     P   P
+  J     PPPP 
+J J     P    
+J J     P    
+ JJ     P    
+""")
